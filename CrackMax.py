672c29136dfbb55fb3bdd1c3bd5cd547541fe819
Firdooslofar <87185796+Firdooslofar@users.noreply.github.com>
2021-07-09 20:39:13 +0300

    CrackMax.py

--- CrackMax.py
+++ CrackMax.py
@@ -104,8 +104,8 @@ jalan("\033[1;93m   ┈┈┈┈┈┈┈┈╲┊┊┊┊╱┈┈┈┈┈┈
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈▕╲▂▂╱▏┈┈┈┈┈┈┈┈")
 print "\033[1;93m♡─────╱▔▔▔▔┊┊┊┊▔▔▔▔╲───────♡\033[1;96mLogin gullabi\033[1;95m♡╰──────────•◈•──────────╯♡"
  
-CorrectUsername = "lofarhacker
-CorrectPassword = "lofarhacker
+CorrectUsername = "BLACK
+CorrectPassword = "Mafia
  
 loop = 'true'
 while (loop == 'true'):
